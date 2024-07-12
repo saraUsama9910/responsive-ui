71ec44d1a480b23be5b99d9028ed90e991e40500
saraUsama9910 <sarausama80@gmail.com>
2024-07-12 07:53:10 +0300

    model

--- lib/widgets/latest_transaction.dart
+++ lib/widgets/latest_transaction.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:respons/utils/app_styles.dart';
 
 class LatestTransaction extends StatelessWidget {
   const LatestTransaction({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        Text(
+          'Latest Transaction',
+          style: AppStyles.styleMedium16(
+            context,
+          ),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        
+      ],
+    );
   }
 }
