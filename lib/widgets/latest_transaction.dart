4ef52b8ce909483467bc1da41af61a407a8bb933
saraUsama9910 <sarausama80@gmail.com>
2024-07-12 07:44:57 +0300

    Latest Transaction

---
+++ lib/widgets/latest_transaction.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LatestTransaction extends StatelessWidget {
+  const LatestTransaction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
