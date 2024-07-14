4d03f7618a20384317a8868890ea62fb9659588c
saraUsama9910 <sarausama80@gmail.com>
2024-07-15 02:01:50 +0300

    row

--- lib/widgets/latest_transaction_list_view.dart
+++ lib/widgets/latest_transaction_list_view.dart
@@ -24,6 +24,7 @@ class LatestTransactionListView extends StatelessWidget {
   ];
   @override
   Widget build(BuildContext context) {
+    return Row();
     return SizedBox(
       height: 80,
       child: ListView.builder(
