448db678b5c18c5c2c175aac6182688c7f4333d1
saraUsama9910 <sarausama80@gmail.com>
2024-08-18 21:26:30 +0300

    ,

--- lib/views/responsive_view.dart
+++ lib/views/responsive_view.dart
@@ -1,22 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:respons/widgets/adaptive_layout_widget.dart';
+import 'package:respons/widgets/custom_drawer.dart';
 import 'package:respons/widgets/responsive_desktop_layout.dart';
 import 'package:respons/widgets/responsive_mobile_layout.dart';
 import 'package:respons/widgets/responsive_tablet_layout.dart';
 
-class ResponsiveView extends StatelessWidget {
+class ResponsiveView extends StatefulWidget {
   const ResponsiveView({super.key});
 
   @override
+  State<ResponsiveView> createState() => _ResponsiveViewState();
+}
+
+class _ResponsiveViewState extends State<ResponsiveView> {
+  final GlobalKey<ScaffoldState> scafoldKey = GlobalKey();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scafoldKey,
       appBar: MediaQuery.sizeOf(context).width < 800
           ? AppBar(
               elevation: 0,
               backgroundColor: const Color(0xFFFAFAFA),
-              leading: const Icon(Icons.menu),
+              leading: IconButton(
+                  onPressed: () {
+                    scafoldKey.currentState!.openDrawer();
+                  },
+                  icon: const Icon(Icons.menu)),
             )
           : null,
+      drawer:
+          MediaQuery.sizeOf(context).width < 800 ? const CustomDrawer() : null,
       backgroundColor: const Color(0xFFF7F9FA),
       body: AdaptiveLayout(
         mobileLayout: (context) => const ResponsiveMobileLayout(),
