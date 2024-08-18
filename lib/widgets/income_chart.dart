d9c51d582a199b443d0709442ad825784e132ad4
saraUsama9910 <sarausama80@gmail.com>
2024-08-18 15:57:26 +0300

    done

--- lib/widgets/income_chart.dart
+++ lib/widgets/income_chart.dart
@@ -1,11 +1,15 @@
-import 'dart:developer';
-
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
-class IncomeChart extends StatelessWidget {
+class IncomeChart extends StatefulWidget {
   const IncomeChart({super.key});
 
+  @override
+  State<IncomeChart> createState() => _IncomeChartState();
+}
+
+class _IncomeChartState extends State<IncomeChart> {
+  int activeIndex = -1;
   @override
   Widget build(BuildContext context) {
     return PieChart(getChartData());
@@ -17,31 +21,33 @@ class IncomeChart extends StatelessWidget {
         pieTouchData: PieTouchData(
           enabled: true,
           touchCallback: (p0, pietouchResponse) {
-            log(pietouchResponse.toString());
+            activeIndex =
+                pietouchResponse?.touchedSection?.touchedSectionIndex ?? -1;
+            setState(() {});
           },
         ),
         sections: [
           PieChartSectionData(
             showTitle: false,
             value: 40,
-            radius: 50,
+            radius: activeIndex == 0 ? 60 : 50,
             color: const Color(0xFF208BC7),
           ),
           PieChartSectionData(
             showTitle: false,
             value: 25,
-            radius: 50,
+            radius: activeIndex == 1 ? 60 : 50,
             color: const Color(0xFF4EB7F2),
           ),
           PieChartSectionData(
             showTitle: false,
             value: 20,
-            radius: 50,
+            radius: activeIndex == 2 ? 60 : 50,
             color: const Color(0xFF064061),
           ),
           PieChartSectionData(
             showTitle: false,
-            radius: 50,
+            radius: activeIndex == 3 ? 60 : 50,
             value: 22,
             color: const Color(0xFFE2DECD),
           ),
