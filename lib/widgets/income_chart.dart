749bb34fb58934b98d37b6808c8bb2747f54a50b
saraUsama9910 <sarausama80@gmail.com>
2024-08-18 15:49:25 +0300

    FlTouchEvent

--- lib/widgets/income_chart.dart
+++ lib/widgets/income_chart.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
@@ -10,27 +12,39 @@ class IncomeChart extends StatelessWidget {
   }
 
   PieChartData getChartData() {
-    return PieChartData(sectionsSpace: 0, sections: [
-      PieChartSectionData(
-        showTitle: false,
-        value: 40,
-        color: const Color(0xFF208BC7),
-      ),
-      PieChartSectionData(
-        showTitle: false,
-        value: 25,
-        color: const Color(0xFF4EB7F2),
-      ),
-      PieChartSectionData(
-        showTitle: false,
-        value: 20,
-        color: const Color(0xFF064061),
-      ),
-      PieChartSectionData(
-        showTitle: false,
-        value: 22,
-        color: const Color(0xFFE2DECD),
-      ),
-    ]);
+    return PieChartData(
+        sectionsSpace: 0,
+        pieTouchData: PieTouchData(
+          enabled: true,
+          touchCallback: (p0, pietouchResponse) {
+            log(pietouchResponse.toString());
+          },
+        ),
+        sections: [
+          PieChartSectionData(
+            showTitle: false,
+            value: 40,
+            radius: 50,
+            color: const Color(0xFF208BC7),
+          ),
+          PieChartSectionData(
+            showTitle: false,
+            value: 25,
+            radius: 50,
+            color: const Color(0xFF4EB7F2),
+          ),
+          PieChartSectionData(
+            showTitle: false,
+            value: 20,
+            radius: 50,
+            color: const Color(0xFF064061),
+          ),
+          PieChartSectionData(
+            showTitle: false,
+            radius: 50,
+            value: 22,
+            color: const Color(0xFFE2DECD),
+          ),
+        ]);
   }
 }
