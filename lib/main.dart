2a5beb144b703ca0ac769011c8bb89565a00893f
saraUsama9910 <sarausama80@gmail.com>
2024-08-21 12:03:28 +0300

    preview now

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:respons/views/responsive_view.dart';
 
 void main() {
-  runApp(const ResponsiveUI());
+  runApp(
+      DevicePreview(enabled: true, builder: (context) => const ResponsiveUI()));
 }
 
 class ResponsiveUI extends StatelessWidget {
   const ResponsiveUI({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
-      home: ResponsiveView(),
+      home: const ResponsiveView(),
     );
   }
 }
